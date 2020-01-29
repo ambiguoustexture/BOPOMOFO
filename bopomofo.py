89a43e9b59cb466f4c051e1e4434a9c7c86222c9
ambiguioustexture <rssermei@gmail.com>
2020-01-30 03:15:50 +0800

    Update bopomofo.py

--- bopomofo.py
+++ bopomofo.py
@@ -4,13 +4,13 @@
 from pypinyin import pinyin, lazy_pinyin, Style
 import re
 
-def chinese_character_2_bopomofo(chinesecharacters):
+def chinese_characters_2_bopomofo(chinese_characters):
     """
     将汉字转换为注音符号表示
     :param chinesecharacters: 待转换的汉字字符串
     :return:                  对应的注音符号
     """
-    return re.sub(r'[\[\]\'\', ]', '', str(pinyin(chinesecharacters, style=Style.BOPOMOFO)))
+    return re.sub(r'[\[\]\'\', ]', '', str(pinyin(chinese_characters, style=Style.BOPOMOFO)))
     #通过pinyin( , style=Style.BOPOMOFO)得到每个汉字对应的注音串列表
     #通过str()将注音串列表转为字符串格式
     #通过re.sub(r'[\[\]\'\', ]', '', str)正则匹配方括号并通过替换为空字符的方式连接起来
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     
     for line in open("./pinyin.txt"):
         index = line.find("=")
-        bopomofo = chinese_character_2_bopomofo(line[ : index])
+        bopomofo = chinese_characters_2_bopomofo(line[ : index])
         line = line[ : index + 1] + bopomofo
         print(line)
         with open("./bopomofo.txt", 'a+') as bopomofodic:
